d8f506fbb28cc26a4bffbf540ee29fd16d656fae
Zisco2002 <zisco2002@gmail.com>
2024-10-12 22:02:28 +0300

    link updates

--- test_graph_dsp.py
+++ test_graph_dsp.py
@@ -168,59 +168,7 @@ class MainWindow(Ui_MainWindow):
             
             
             
-    def updatePlotLinked(self):
-        # for the first graph:
-        if self.current_index < len(self.browsedData_y):
-            # Determine the range of data to plot in this frame
-            end_index = min(self.current_index + self.chunk_size, len(self.browsedData_y))
-            segment_x = self.data_x[self.current_index:end_index]
-            segment_y = self.browsedData_y[self.current_index:end_index]
-            # Update the plot with new data
-            self.graphWidget.plot(segment_x, segment_y, pen='b', clear=False)
-            # updating the view to follow the signal until reaching the end of the signal so the graph wouldn't expand
-            if end_index != len(self.browsedData_y):
-                self.graphWidget.plotItem.setXRange(
-                    self.current_index,
-                    end_index,
-                    padding=0
-                )
-                updateViewStep = 10
-                self.current_index+= updateViewStep
-            else:
-                self.timer.stop()
-                self.current_index = 0    
-            self.graphWidget.plotItem.setYRange(-1, 1, padding = 0)
-        
-        else:
-            self.timer.stop()  # Stop the timer when the end is reached
-            self.current_index = 0  # resetting the starting index
-        
-        # same for the second graph:
-        if self.current_index_2 < len(self.browsedData_y_2):
-            # Determine the range of data to plot in this frame
-            end_index_2 = min(self.current_index_2 + self.chunk_size, len(self.browsedData_y_2))
-            segment_x_2 = self.data_x_2[self.current_index_2:end_index_2]
-            segment_y_2 = self.browsedData_y_2[self.current_index_2:end_index_2]
-            # Update the plot with new data
-            self.graphWidget_2.plot(segment_x_2, segment_y_2, pen='b', clear=False)
-            # updating the view to follow the signal until reaching the end of the signal so the graph wouldn't expand
-            if end_index_2 != len(self.browsedData_y_2):
-                self.graphWidget_2.plotItem.setXRange(
-                    self.current_index_2,
-                    end_index_2,
-                    padding=0
-                )
-                updateViewStep_2 = 10
-                self.current_index_2+= updateViewStep_2
-            else:
-                self.timer_2.stop()
-                self.current_index_2 = 0    
-                self.graphWidget_2.plotItem.setYRange(-1, 1, padding = 0)
-        
-        else:
-            self.timer_2.stop()  # Stop the timer when the end is reached
-            self.current_index_2 = 0
-        
+    
             
             
     def pauseTheSignal(self):
