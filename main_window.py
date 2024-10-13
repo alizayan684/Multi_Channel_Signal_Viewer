8187203e253143926d87f817d97ca85e55a20254
alizayan684 <alizayan684@gmail.com>
2024-10-14 02:14:54 +0300

    solved a mistake

--- main_window.py
+++ main_window.py
@@ -8,31 +8,32 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+from PyQt6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+from PyQt6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QCursor, QFont, QFontDatabase, QGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QGridLayout,
+from PyQt6.QtWidgets import (QApplication, QComboBox, QFrame, QGridLayout,
     QHBoxLayout, QLabel, QLayout, QLineEdit,
     QListWidget, QListWidgetItem, QMainWindow, QPushButton,
     QScrollBar, QSizePolicy, QSlider, QVBoxLayout,
-    QWidget)
+    QWidget, QMainWindow)
 
 from pyqtgraph import PlotWidget
 
-class Ui_SignalViewer(object):
+class Ui_SignalViewer(QMainWindow):
     def setupUi(self, SignalViewer):
-        if not SignalViewer.objectName():
-            SignalViewer.setObjectName(u"SignalViewer")
-        SignalViewer.resize(1057, 754)
+        SignalViewer.setObjectName(u"SignalViewer")
+        SignalViewer.resize(1000, 800)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(SignalViewer.sizePolicy().hasHeightForWidth())
+        # enable resizing of the window
+        SignalViewer.setWindowFlag(Qt.WindowType.WindowMaximizeButtonHint, True)
         SignalViewer.setSizePolicy(sizePolicy)
         SignalViewer.setStyleSheet(u"/*\n"
 "Material Dark Style Sheet for QT Applications\n"
